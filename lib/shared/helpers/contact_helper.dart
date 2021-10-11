e3e25cb2147c4f192436ab2b841dcec72596fabe
CodeAkio <siqueira_2004@hotmail.com>
2021-10-11 14:03:56 -0300

    CRUD contact helper

--- lib/shared/helpers/contact_helper.dart
+++ lib/shared/helpers/contact_helper.dart
@@ -41,8 +41,73 @@ class ContactHelper {
             "$nameColumn VARCHAR, "
             "$emailColumn VARCHAR, "
             "$phoneColumn VARCHAR, "
-            "$imageColumn VARCHAR)");
+            "$imgColumn VARCHAR)");
       },
     );
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+
+    return contact;
+  }
+
+  Future<Contact?> getContact(int id) async {
+    Database dbContact = await db;
+
+    List<Map<String, dynamic>> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
+
+    if (maps.isNotEmpty) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+
+    return await dbContact
+        .delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+
+    return await dbContact.update(contactTable, contact.toMap(),
+        where: "$idColumn = ?", whereArgs: [contact.id]);
+  }
+
+  Future<List<Contact>> getAllContacts() async {
+    Database dbContact = await db;
+
+    List<Map<String, dynamic>> listMap =
+        await dbContact.rawQuery("SELECT * FROM $contactTable");
+
+    List<Contact> listContact = [];
+
+    for (var m in listMap) {
+      listContact.add(Contact.fromMap(m));
+    }
+
+    return listContact;
+  }
+
+  Future<int?> getNumber() async {
+    Database dbContact = await db;
+
+    return Sqflite.firstIntValue(
+        await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+  Future close() async {
+    Database dbContact = await db;
+
+    dbContact.close();
   }
 }
